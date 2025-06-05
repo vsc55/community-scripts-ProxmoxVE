f0849a43b77bd32c0fbda87914ac48793fdce80a
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-06-05 13:28:05 +0200

    Update wger-install.sh

--- install/wger-install.sh
+++ install/wger-install.sh
@@ -38,12 +38,12 @@ curl -fsSL "https://github.com/wger-project/wger/archive/refs/tags/$RELEASE.tar.
 tar xzf $RELEASE.tar.gz
 mv wger-$RELEASE /home/wger/src
 cd /home/wger/src
-$STD /home/wger/.venv/bin/wger create-settings --database-path /home/wger/db/database.sqlite
 $STD uv venv /home/wger/.venv
 $STD /home/wger/.venv/bin/python -m ensurepip --upgrade
 $STD /home/wger/.venv/bin/python -m pip install --upgrade pip
 $STD /home/wger/.venv/bin/python -m pip install -r requirements_prod.txt
 $STD /home/wger/.venv/bin/python -m pip install -e .
+$STD /home/wger/.venv/bin/wger create-settings --database-path /home/wger/db/database.sqlite
 sed -i "s#home/wger/src/media#home/wger/media#g" /home/wger/src/settings.py
 sed -i "/MEDIA_ROOT = '\/home\/wger\/media'/a STATIC_ROOT = '/home/wger/static'" /home/wger/src/settings.py
 $STD /home/wger/.venv/bin/wger bootstrap
